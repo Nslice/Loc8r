778983903bda1cad57442558f58322ccce13b540
Talgat <tairovtalgat@gmail.com>
2021-06-25 17:34:16 +0600

    init

---
+++ app_server/routes/index.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const ctrlMain = require("../controllers/main");
+
+const router = express.Router();
+router.get("/", ctrlMain.index);
+
+
+module.exports = router;
